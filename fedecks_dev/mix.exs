1f80f34ec2000e07cabaef7ce7af9518994c09a7
Paul Wilson <paulanthonywilson@gmail.com>
2022-11-30 15:36:48 +0000

    dialyzer

--- fedecks_dev/mix.exs
+++ fedecks_dev/mix.exs
@@ -43,7 +43,8 @@ defmodule FedecksDev.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:websocket_client, git: "git@github.com:vtm9/websocket_client.git"},
       {:credo, "~> 1.6", only: :dev},
-      {:recon, "~> 2.5"}
+      {:recon, "~> 2.5"},
+      {:dialyxir, "~> 1.2", only: [:dev, :test]}
     ]
   end
 
